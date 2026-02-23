5f1b02c5976146761a116a03d5ab7f46cd49ac64
ZiyuanLi-Econ <1796494629@qq.com>
2026-02-23 15:52:13 +0100

    Add files via upload

---
+++ Day2--23.02.2026.py
@@ -0,0 +1,77 @@
+message="LLL"
+print(message)
+message="love hepburn"
+print(message.title())
+print(message.upper())
+print(message.lower())
+
+first_name="Ziyuan"
+last_name="Li"
+full_name=first_name+" "+last_name
+print(full_name)
+print(full_name.upper())
+print(full_name.lower())
+print(full_name.title())
+
+fname="Ziyuan"+" "+"Li"
+print(fname)
+
+fname="Ziyuan"+" "+"Li "
+fname.rstrip()
+fname
+
+print("Ziyuan \nLi\nmi\nli\nss\nrr")
+print("Ziyuan\tLi")
+print(" Ziyuan  Li ".strip()+" MM".strip())
+
+message='LET''GO' #plus doent affect at all
+print(message)
+
+name="Ziyuan Li"
+print("hello Eric, would you like to learn python?")
+print(name.upper())
+
+print("Albert Einstein once said, ""\"A person should try everything\"")
+name="Albert Einstein"
+print(name + " once said," "\"A person\"")
+
+name=" Ziyuan Li "
+print(name)
+print(name.strip())
+print(name.rstrip())
+print(name.lstrip())
+
+4/2
+4*2
+4**2
+4+2
+100000*23**977
+
+x=5
+while x < 100:
+ if x/2==1:
+    x=x+3
+ else:
+   x=x+5
+ print(x)
+
+0.1**0.5
+0.2+0.1
+
+
+age=2
+while age< 10:
+ message="HAPPY "+str(age)+"rd birthday"
+ print(message)
+ age=age+1
+
+3/2
+
+import this
+
+
+number="5"
+x=int(number)
+while x<=100:
+ print(x)
+ x=x+1
