22c98e6c7f7638bf2282601d606f7f69126fc03a
ZiyuanLi-Econ <1796494629@qq.com>
2026-03-12 15:24:46 +0100

    Add files via upload

---
+++ Day13--12.03.2026.py
@@ -0,0 +1,36 @@
+import numpy as np
+my_list=[1,2,3]
+arr = np.array(my_list)
+numbers = [[1,2,3],[4,5,6]]
+arr2 = np.array(numbers)
+arr2
+np.arange(0,10)
+np.arange(0,10,2)
+np.zeros(10)
+np.zeros((10,10))
+np.zeros((1,5))
+np.ones((5,8))
+np.linspace(0,5,100)
+np.eye(4)
+np.eye(9)
+np.ones(9)
+np.random.rand(5)
+np.random.rand(5,10)
+np.arange(1,10)
+np.random.rand(3,3)
+xx = np.random.randn(100)
+np.random.randint(1,100,100)
+arr = np.arange(25)
+arr
+ranarr=np.random.randint(0,50,10)
+ranarr
+arr = arr.reshape(5,5)
+ranarr.max()
+arr.min()
+arr.argmax()
+arr.argmin()
+arr
+ranarr.argmax()
+arr.shape
+from numpy.random import randint
+randint(1,10)
